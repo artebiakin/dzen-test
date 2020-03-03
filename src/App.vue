1ac06587420703eed07884230174acbfbe17fa54
Oleksandr Artebiakin <oleksandrartebiakin@gmail.com>
2020-03-03 17:56:33 +0200

    first task is done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,155 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header-component />
+    <div class="window">
+      <section>
+        <form @submit.prevent="onSubmit">
+          <h3>IP Address</h3>
+          <input pattern="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" placeholder="000.000.00.00" />
+          <div class="btn">Get information</div>
+        </form>
+      </section>
+      <section>
+        <h3>Result</h3>
+        <div class="grid">
+          <div>IP address</div>
+          <div>Continent/code</div>
+          <div>Country/code</div>
+          <div>City</div>
+          <div>Postcode</div>
+          <div>Coordinates</div>
+          <div>000.000.00.00</div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </section>
+      <section>
+        <h3>History</h3>
+        <div class="grid">
+          <div>IP address</div>
+          <div>Country/code</div>
+          <div>City</div>
+          <div>000.000.00.00</div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="btn btn-disabled">Clear history</div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    'header-component': Header,
   },
 };
 </script>
 
 <style lang="scss">
+@import '@/assets/scss/ui-kit.scss';
+body {
+  min-width: 1280px;
+  max-width: 2560px;
+  display: flex;
+  justify-content: center;
+  background-color: #ffffff;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  max-width: 1170px;
+  margin-top: 48px;
+}
+.window {
+  background-color: #ffffff;
+  box-shadow: 0px 0px 50px rgba(230, 230, 230, 0.8);
+  width: 936px;
+  height: auto;
+  margin: 0 auto;
+  margin-top: 57px;
+  & > :first-child {
+    padding-top: 45px;
+  }
+  & section {
+    padding: 48px;
+    padding-top: 0;
+    & h3 {
+      margin: 0;
+      font-family: Open Sans;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 18px;
+      line-height: 25px;
+      color: #000000;
+    }
+    & form {
+      & input {
+        margin: 8px 0;
+        width: 396px;
+      }
+      & .btn {
+        margin-top: 10px;
+      }
+    }
+    & .grid {
+      display: grid;
+      border: 1px solid #cccccc;
+      box-sizing: border-box;
+      border-radius: 8px;
+      grid-auto-rows: auto;
+      overflow: hidden;
+      margin-top: 13px;
+      margin-bottom: 18px;
+      grid-template-rows: repeat(2, 34px);
+      & :nth-child(n) {
+        padding: 9px 18px;
+        text-align: center;
+        color: #ffffff;
+        font-family: Open Sans;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 12px;
+        line-height: 16px;
+      }
+    }
+  }
+  & :nth-child(2) {
+    & .grid {
+      grid-template-columns: repeat(6, auto);
+      & :nth-child(-n + 6) {
+        background-color: #656bf2;
+      }
+      & :nth-child(n + 7):nth-child(n) {
+        color: #969696;
+      }
+      & :nth-child(n + 8):nth-child(n) {
+        border-left: 1px solid #cccccc;
+        color: #969696;
+      }
+    }
+  }
+  & :nth-child(3) {
+    & .grid {
+      width: 50%;
+      grid-template-columns: repeat(3, auto);
+      & :nth-child(-n + 3) {
+        background-color: #656bf2;
+      }
+      & :nth-child(n + 4):nth-child(n) {
+        color: #969696;
+      }
+      & :nth-child(n + 5):nth-child(n) {
+        border-left: 1px solid #cccccc;
+        color: #969696;
+      }
+    }
+  }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,110 @@
+<template>
+  <header>
+    <div class="row">
+      <div class="col"></div>
+      <div class="col logo">GeoIP</div>
+      <div class="col">
+        <div class="switch">
+          <span>EN</span>
+          <svg
+            width="10"
+            height="7"
+            viewBox="0 0 10 7"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="arrow-up"
+          >
+            <path d="M5 8.74228e-07L9.33012 6.75L0.669872 6.75L5 8.74228e-07Z" fill="white" />
+          </svg>
+          <div class="switch--active">
+            <ul>
+              <li>RU</li>
+              <li class="active">EN</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 48px;
+  background-color: #656bf2;
+  color: #fff;
+  & .row {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 1170px;
+    margin: 0 auto;
+    height: 100%;
+    & .col {
+      width: 33.3%;
+    }
+    & .logo {
+      font-family: Open Sans;
+      font-style: normal;
+      font-weight: 800;
+      font-size: 18px;
+      line-height: 25px;
+      text-align: center;
+      cursor: pointer;
+    }
+    & .switch {
+      display: block;
+      float: right;
+      cursor: pointer;
+      position: relative;
+      padding: 5px;
+      display: flex;
+      align-items: center;
+      &:hover {
+        & .switch--active {
+          display: block;
+        }
+      }
+      &--active {
+        position: absolute;
+        top: 100%;
+        left: 7px;
+        background-color: #ffffff;
+        box-shadow: 0px 20px 50px rgba(229, 229, 229, 0.8);
+        border-radius: 8px;
+        display: none;
+        & ul {
+          color: #000;
+          list-style: none;
+          padding: 0;
+          margin: 0;
+          padding: 7px 8px;
+          & li {
+            margin: 7px;
+            font-family: Open Sans;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 18px;
+            line-height: 25px;
+          }
+          & .active {
+            color: #656bf2;
+          }
+        }
+      }
+      & span {
+        font-family: Open Sans;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 25px;
+        margin-right: 4px;
+      }
+    }
+  }
+}
+</style>
